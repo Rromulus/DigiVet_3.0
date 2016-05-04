d2fe9800e6c3fc17cba3c463c2451a1febb6b8f4
Rromulus <romy_blankendaal@hotmail.com>
2016-05-04 12:18:14 +0200

    DigiVet 2.0
    This folder contains DigiVet 2.0, the visual version of DigiVet

---
+++ Digivet_2_0/contact.php
@@ -0,0 +1,21 @@
+<!-- this is the result screen --> 
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>DigiVet</title>
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+<h1> Contact details Veterinarian</h1>
+
+<p> Please call Medior vet:</p>
+<p>Abubakar Zibuila</p>
+<p>Telephone number: 026586919</p>
+
+
+
+</body>
+</html> 	
